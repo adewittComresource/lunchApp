8717e793642f33bc4df879e53aba4db7144ff900
rhelvey <rhelvey@comresource.com>
2015-06-12 10:17:56 -0400

    formvalidationforinsertlunchlocation

--- src/main/java/com/comresource/lunchapp/models/Users.java
+++ src/main/java/com/comresource/lunchapp/models/Users.java
@@ -10,17 +10,24 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "USERS")
 public class Users implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
     @Column(name = "USER_ID")
     private String userID;
     @Column(name = "USER_NAME")
     private String userName;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "SALT")
+    private String salt;
 
     //userID
+
     public String getUserID() {
         return userID;
     }
+
     public void setUserID(String userID) {
         this.userID = userID;
     }
@@ -29,6 +36,7 @@ public class Users implements Serializable {
     public String getUserName() {
         return userName;
     }
+
     public void setUserName(String userName) {
         this.userName = userName;
     }
@@ -36,6 +44,22 @@ public class Users implements Serializable {
     public void update(String userId, String name) {
         this.userID = userId;
         this.userName = name;
-        
+
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSALT() {
+        return salt;
+    }
+
+    public void setSALT(String salt) {
+        this.salt = salt;
     }
 }
