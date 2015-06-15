28976bcca6376aee1146af055beb542653f23ae0
Randy Meyer <meyer.randy@gmail.com>
2015-06-15 10:33:28 -0400

    removed the dependancy on the hardcoded port and host name

--- src/main/java/com/comresource/lunchapp/resources/CheckAuthentication.java
+++ src/main/java/com/comresource/lunchapp/resources/CheckAuthentication.java
@@ -1,16 +1,10 @@
 package com.comresource.lunchapp.resources;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.core.util.MultivaluedMapImpl;
-import java.io.FileNotFoundException;
+import com.comresource.lunchapp.PersistenceManager;
+import com.comresource.lunchapp.models.Users;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.util.List;
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -22,88 +16,69 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 import org.apache.commons.codec.digest.DigestUtils;
-
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 @Path("/checkAuthentication")
 public class CheckAuthentication {
-    String BASE_WEBSERVER = "http://localhost";
-    String BASE_PORT = "8084";
-    private static Client client;
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public String go(String credentials, @Context HttpServletRequest httpRequest, @Context HttpServletResponse httpResponse) throws Exception {
-        
-        
-        //Create Jersey Client
-        client = Client.create();
+
         //Check if already logged in
         //Create a Session
         HttpSession session = httpRequest.getSession(true);
         String authenticated = (String) session.getAttribute("authenticated");
-        
+
         String authResult = "passed";
         if (authenticated == null || authenticated.equals("false")) { //Not Authenticated
             //Get Username and password 
             //parse JSON object
-             
+
             JSONObject openJSON = (JSONObject) JSONSerializer.toJSON(credentials);
             String loginUserName = (String) openJSON.get("userName");
-            String clientPassword = openJSON.getString("password");         
-                   
-            if (loginUserName != null) {
-                //Get properties for server port and authType
-                 authResult = Authentication(loginUserName, clientPassword, session, httpResponse);
+            String clientPassword = openJSON.getString("password");
 
+            if (loginUserName != null) {
+                authResult = Authentication(loginUserName, clientPassword, session, httpResponse);
             } else {
                 //redirect to index page
-                RequestDispatcher page = httpRequest.getRequestDispatcher("/loginPage.jsp");
+                RequestDispatcher page = httpRequest.getRequestDispatcher("loginPage.jsp");
                 page.forward(httpRequest, httpResponse);
             }
-        } 
+        }
         return authResult;
     }
 
-    public  String Authentication(String loginUserName, String clientPassword, HttpSession session, HttpServletResponse httpResponse) throws Exception {
-        //Get HTTP session
-        String usersURI = BASE_WEBSERVER + ":" + BASE_PORT + "/lunchApp/services/users";
-        WebResource usersResource = client.resource(usersURI);
-        //EntityId parameter
-        MultivaluedMap queryParams = new MultivaluedMapImpl();
-        queryParams.add("sortBy", "+userName");
+    public String Authentication(String loginUserName, String clientPassword, HttpSession session, HttpServletResponse httpResponse) throws Exception {
+        EntityManager entityManager = PersistenceManager.getEntityManager();
+        //Get Session to act as a factory for the Criteria Instance
+        Session sess = entityManager.unwrap(Session.class);
+        Criteria crit = sess.createCriteria(Users.class).add(Restrictions.eq("userName", loginUserName));
+        //Query for Results
+        List results = crit.list();
 
-        // Get Data Flow Params
-        ClientResponse usersResponse = usersResource.queryParams(queryParams).accept("application/json").get(ClientResponse.class);
-        //Turn Response to JSON object
-        JSONArray usersJSON = (JSONArray) JSONSerializer.toJSON(usersResponse.getEntity(String.class));
+        if (results != null && !results.isEmpty()) {
+            // disconnect the entity manager
+            entityManager.detach(results);
 
-        int usersJSONSize = usersJSON.size();
-        
-        for(int i = 0; i < usersJSONSize; i++){
-            JSONObject userObject = usersJSON.getJSONObject(i);
-            String serverUsername = userObject.getString("userName");
-            String serverPassword = userObject.getString("password");
-            
-            //check the user entered password and username with server's hash password
+            // get the row from the DB and turn it into the User object
+            Users userObj = (Users) results.get(0);
+            String serverPassword = userObj.getPassword();
             boolean checkHashResult = checkHashPassword(serverPassword, loginUserName, clientPassword);
-           
-            if(checkHashResult){
+            if (checkHashResult) {
                 session.setAttribute("authenticated", "true");
                 return "passed";
             }
-            
         }
-        
         return "failed";
-       
     }
-
-
 
     public static String checkAuthenticatedSession(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws ServletException, IOException {
         //get the session variable
@@ -112,21 +87,20 @@ public class CheckAuthentication {
         String authed = "false";
         if ("false".equals(authenticated) || authenticated == null) {
             //redirect to login page
-            httpResponse.sendRedirect("/lunchApp/loginPage.jsp");
+            httpResponse.sendRedirect("loginPage.jsp");
         } else {
             authed = "true";
         }
         return authed;
     }
-    
-    public boolean checkHashPassword(String serverPasswordHash, String clientLoginUserName, String clientLoginPassword){
-       
+
+    public boolean checkHashPassword(String serverPasswordHash, String clientLoginUserName, String clientLoginPassword) {
+
         //hash user entered password and username
-        String clientSalt = DigestUtils.sha256Hex(clientLoginUserName);     
+        String clientSalt = DigestUtils.sha256Hex(clientLoginUserName);
         String clientLoginPasswordHash = DigestUtils.sha256Hex(clientLoginPassword + clientSalt);
-        
+
         //return true if server's password hash is equal to user entered password hash
         return serverPasswordHash.equals(clientLoginPasswordHash);
-        
     }
 }
