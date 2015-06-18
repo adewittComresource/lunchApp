5e1e27448c4a076a100d703f2e37046cdbd49d58
Randy Meyer <meyer.randy@gmail.com>
2015-06-17 21:37:30 -0400

    Uses a named native query.  I still want to clean up the query but it's working and returning the correct data so we can work on the front end JS.

--- src/main/java/com/comresource/lunchapp/resources/RestaurantGraphInfo.java
+++ src/main/java/com/comresource/lunchapp/resources/RestaurantGraphInfo.java
@@ -1,12 +1,7 @@
 package com.comresource.lunchapp.resources;
 
 import com.comresource.lunchapp.PersistenceManager;
-import com.comresource.lunchapp.models.VSuggestProfile;
-import static java.lang.System.console;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Collection;
-import java.util.Date;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -17,10 +12,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
 
 @Path("/restaurantgraphinfo")
 public class RestaurantGraphInfo {
@@ -28,30 +21,19 @@ public class RestaurantGraphInfo {
     @GET
     @Produces({"application/json"})
     @Consumes({"application/json"})
-    public Response getAll(@Context HttpServletRequest httpRequest)
-            throws Exception {
-        HttpSession session = httpRequest.getSession(true);
+    public Response getAll(@Context HttpServletRequest httpRequest) throws Exception {
+
+        String user = httpRequest.getSession(true).getAttribute("userName").toString();
         EntityManager entityManager = PersistenceManager.getEntityManager();
-        Session sess = (Session) entityManager.unwrap(Session.class);
-        //DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = new Date();
-        String testingUserName = (String)session.getAttribute("userName");
-        Criteria crit;
-        crit = sess.createCriteria(VSuggestProfile.class).add(Restrictions.conjunction()
-                .add(
-                    Restrictions.disjunction()
-                        .add(Restrictions.eq("userName", testingUserName))
-                        .add(Restrictions.isNull("userName"))
-                )
-                .add(Restrictions.eq("suggestDate", date))).addOrder(Order.desc("userName"));
+        Session sess = entityManager.unwrap(Session.class);
+
+        Query query = sess.getNamedQuery("suggestionForUser");
         
-        //Only get the first 3 rows
-//        crit.setFirstResult(0).setMaxResults(3);
-        
-        Collection<?> results = crit.list();
-        if (results != null) {
-            entityManager.detach(results);
-        }
+        // Setting the user name in the query twice, It's not a mistake
+        query.setParameter(0, user);
+        query.setParameter(1, user);
+        List results = query.list();
+
         Response.ResponseBuilder builder = Response.ok(results);
         if (builder == null) {
             throw new Exception("builder == null");
