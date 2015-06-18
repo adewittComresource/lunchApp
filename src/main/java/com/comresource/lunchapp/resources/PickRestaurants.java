69ded19f9a50f942ee206427f4ca5373501c4079
rhelvey <rhelvey@comresource.com>
2015-06-18 15:52:44 -0400

    praying, who knew computers would lead one to prayer

--- src/main/java/com/comresource/lunchapp/resources/PickRestaurants.java
+++ src/main/java/com/comresource/lunchapp/resources/PickRestaurants.java
@@ -2,37 +2,34 @@ package com.comresource.lunchapp.resources;
 
 import com.comresource.lunchapp.models.Restaurants;
 import com.comresource.lunchapp.PersistenceManager;
-import com.comresource.lunchapp.models.Is_Open;
 import com.comresource.lunchapp.models.SuggestHistory;
 import static com.comresource.lunchapp.resources.InsertRestaurants.log;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Restrictions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
- 
+
 // DOcs on Path annotation
 // http://docs.oracle.com/cd/E19776-01/820-4867/6nga7f5nc/index.html
 @Path("/pickRestaurants")
 public class PickRestaurants {
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAll() throws Exception {
@@ -42,20 +39,21 @@ public class PickRestaurants {
         Session sess = entityManager.unwrap(Session.class);
         Criteria crit = sess.createCriteria(Restaurants.class);
         //Query for Results
-        Collection<?> results = crit.list();
+        List results = new ArrayList();
+        results = crit.list();
 
-        int currentSize = results.size();
-        int randomNumber = (int)(Math.random() * currentSize + 1);
-        int counter = 1;
+        Collections.shuffle(results);
+        
+        int maxValue = 3;
+        int counter = 0;
 
         Iterator iterator = results.iterator();
-        while(iterator.hasNext()){
-            Restaurants currentRestaurant = (Restaurants)iterator.next();
-            
-            if(counter == randomNumber){
+        while (iterator.hasNext() && counter < 3) {
+            Restaurants currentRestaurant = (Restaurants) iterator.next();
+            if (counter <= maxValue) {
                 String restaurantId = currentRestaurant.getRestaurantId();
                 String name = currentRestaurant.getName();
-                insertSuggestion(restaurantId,entityManager);
+                insertSuggestion(restaurantId, entityManager);
             }
             counter += 1;
         }
@@ -67,19 +65,19 @@ public class PickRestaurants {
         builder = Response.ok(results);
         if (builder == null) {
             throw new Exception("builder == null");
-            
+
         }
         CacheControl cacheControl = new CacheControl();
         cacheControl.setNoCache(true);
         return builder.cacheControl(cacheControl).build();
     }
-    
-    public void insertSuggestion(String restaurantId,EntityManager entityManager) throws Exception {
+
+    public void insertSuggestion(String restaurantId, EntityManager entityManager) throws Exception {
         SuggestHistory currentSuggestion = new SuggestHistory();
         String suggestHistoryId = UUID.randomUUID().toString();
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date currentDate = new Date();
-        
+
         currentSuggestion.setSuggestId(suggestHistoryId);
         currentSuggestion.setRestaurantId(restaurantId);
         currentSuggestion.setSuggestDate(currentDate);
